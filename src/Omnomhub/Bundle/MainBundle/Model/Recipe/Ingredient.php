31c747db7fe20d1562180236ae5ad93172db35dd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-07-15 20:24:58 +0200

    Ingredient properties

--- src/Omnomhub/Bundle/MainBundle/Model/Recipe/Ingredient.php
+++ src/Omnomhub/Bundle/MainBundle/Model/Recipe/Ingredient.php
@@ -4,4 +4,89 @@ namespace Omnomhub\Bundle\MainBundle\Model\Recipe;
 
 class Ingredient
 {
+    protected $id;
+    protected $name;
+    protected $unit;
+    protected $amount;
+    protected $photo;
+
+    /**
+     * @param mixed $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $photo
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param mixed $unit
+     */
+    public function setUnit($unit)
+    {
+        $this->unit = $unit;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUnit()
+    {
+        return $this->unit;
+    }
 }
