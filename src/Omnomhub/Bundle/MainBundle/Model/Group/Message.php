0087e9866ad2d1cf7927bd801ccfb5670c3457ab
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-07-15 20:28:21 +0200

    Created a message model for group messages

---
+++ src/Omnomhub/Bundle/MainBundle/Model/Group/Message.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Omnomhub\Bundle\MainBundle\Model\Group;
+
+class Message
+{
+    protected $id;
+    protected $author;
+    protected $message;
+    protected $parent;
+    protected $timestamp;
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $parent
+     */
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param mixed $timestamp
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+}
