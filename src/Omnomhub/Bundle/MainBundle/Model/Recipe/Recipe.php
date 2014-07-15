50ad97cb239f4987c9d23562accf8d29d02370ad
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-07-15 20:23:43 +0200

    Recipe properties

--- src/Omnomhub/Bundle/MainBundle/Model/Recipe/Recipe.php
+++ src/Omnomhub/Bundle/MainBundle/Model/Recipe/Recipe.php
@@ -4,4 +4,89 @@ namespace Omnomhub\Bundle\MainBundle\Model\Recipe;
 
 class Recipe
 {
+    protected $id;
+    protected $tile;
+    protected $photo;
+    protected $description;
+    protected $ingredients;
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $ingredients
+     */
+    public function setIngredients($ingredients)
+    {
+        $this->ingredients = $ingredients;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIngredients()
+    {
+        return $this->ingredients;
+    }
+
+    /**
+     * @param mixed $photo
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param mixed $tile
+     */
+    public function setTile($tile)
+    {
+        $this->tile = $tile;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTile()
+    {
+        return $this->tile;
+    }
 }
